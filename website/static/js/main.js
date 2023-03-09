1fa76966c5805a22699cdffd4b8efc6e3a2f8c98
Simon Schneider <schneider_berghausen@web.de>
2023-03-09 06:37:11 +0100

    added Eventlistener to img-3 el's

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -7,3 +7,13 @@ function openSearch(){
 function closeSearch(){
     document.getElementById("myOverlay").style.display = "none";
 }
+
+//making the product-imgs bigger on click
+const imgs = document.getElementsByClassName("img-3");
+let clicked_img = "";
+
+for(let i = 0; i<imgs.length; i++){
+    imgs[i].addEventListener("click",() => {
+        //open new window with big img in the middle
+    })
+}
