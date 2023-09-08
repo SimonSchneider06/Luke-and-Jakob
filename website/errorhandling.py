cf8ee231457eb2bac6458709dacc3d5492cc9881
Simon Schneider <schneider_berghausen@web.de>
2023-09-08 08:38:07 +0200

    new error_code and code cleanup

--- website/errorhandling.py
+++ website/errorhandling.py
@@ -1,17 +1,21 @@
-from flask import render_template
+from flask import render_template,Flask
 from flask_login import current_user
 
-def errors(app):
+def errors(app:Flask):
 
     #404
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template("error.html",user = current_user) , 404
+        return render_template("error.html") , 404
 
     @app.errorhandler(500)
     def internal_server_error(error):
-        return render_template("error.html",user = current_user) , 500  #pragma: no cover , not testable
+        return render_template("error.html") , 500  #pragma: no cover , not testable
 
     @app.errorhandler(403)
     def page_forbidden(error):
-        return render_template("error.html",user = current_user) , 403
+        return render_template("error.html") , 403
+    
+    @app.errorhandler(400)
+    def bad_request(error):
+        return render_template("error.html"), 400
