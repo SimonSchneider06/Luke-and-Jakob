c842ca298d2ef91aee2115408935ffee5fc7085b
Simon Schneider <schneider_berghausen@web.de>
2023-11-09 20:16:44 +0100

    navbar-laptop implemented

--- website/views.py
+++ website/views.py
@@ -6,12 +6,12 @@ from .models import Guitar
 views = Blueprint('views',__name__)
 
 @views.route('/',methods = ['GET'])
-def home():
-    return render_template('home.html')
+def Home():
+    return render_template('home.html', active = "Home")
 
 @views.route("/about_us",methods = ["GET"])
-def about_us():
-    return render_template("about_us.html")
+def About():
+    return render_template("about_us.html", active = "About")
 
 # expired
 # @views.route('/logged_in',methods = ['POST','GET'])
@@ -20,8 +20,12 @@ def about_us():
 #     return render_template("logged_in.html")
 
 @views.route("/services", methods = ["GET"])
-def services():
-    return render_template("services.html")
+def Services():
+    return render_template("services.html", active = "Services")
+
+@views.route("/nachhaltigkeit", methods = ["GET"])
+def Nachhaltigkeit():
+    return render_template("/part_templates/base.html", active = "Nachhaltigkeit")
 
 # @views.route("/design-your-dream",methods = ["GET","POST"])
 # def design_your_dream():
@@ -29,19 +33,21 @@ def services():
 
 # strg + k + c to comment multiple things at once (strg + k + u = uncomment)
 
+
+#--------------------------------------------------for shop site------
 #shopping cart site
-@views.route("/cart",methods = ["GET"])
-def shopping_cart():
-    return render_template("shopping_cart.html")
+# @views.route("/cart",methods = ["GET"])
+# def shopping_cart():
+#     return render_template("shopping_cart.html")
 
 
-@views.route("/shop")
-def shop():
-    products = db.session.query(Guitar).order_by(Guitar.id)
-    return render_template("shop.html",products = products)  
+# @views.route("/shop")
+# def shop():
+#     products = db.session.query(Guitar).order_by(Guitar.id)
+#     return render_template("shop.html",products = products)  
 
-@views.route("/product/<product_name>", methods = ["GET", "POST"]) #String default converter -> werkzeug routing rules
-def product_site(product_name):
-    #Gets product or returns 404 if it doesn't exist
-    product = Guitar.query.filter_by(name = product_name).first_or_404()
-    return render_template("product.html", product = product)
+# @views.route("/product/<product_name>", methods = ["GET", "POST"]) #String default converter -> werkzeug routing rules
+# def product_site(product_name):
+#     #Gets product or returns 404 if it doesn't exist
+#     product = Guitar.query.filter_by(name = product_name).first_or_404()
+#     return render_template("product.html", product = product)
